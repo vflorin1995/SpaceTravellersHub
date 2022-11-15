c714897332eedef86b97fd0a8b6d142a8bf860fa
vflorin1995 <vflorin1995@gmail.com>
2022-11-15 09:52:24 +0200

    Add redux file for missions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import Rockets from './Rockets/rockets';
+import Mission from '../modules/mission';
 
 const rootReducer = combineReducers({
   Rockets,
+  Mission,
 });
 
 const store = configureStore({ reducer: rootReducer });
