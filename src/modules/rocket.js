b313aceb2f876fbd92386a60bf4427ee47503ec4
vflorin1995 <vflorin1995@gmail.com>
2022-11-15 15:43:40 +0200

    Add styling

--- src/modules/rocket.js
+++ src/modules/rocket.js
@@ -3,12 +3,12 @@ import { PropTypes } from 'prop-types';
 const Rocket = ({
   id, name, type, img,
 }) => (
-  <>
+  <div className="flex my-8 mx-8">
+    <img className="w-60" src={img} alt="spaceship" />
     <div>{id}</div>
     <div>{name}</div>
     <div>{type}</div>
-    <img src={img} alt="spaceship" />
-  </>
+  </div>
 );
 
 Rocket.propTypes = {
