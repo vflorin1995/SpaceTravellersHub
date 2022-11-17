057237a7548b65f299347fbca1140ba16385d846
vflorin1995 <vflorin1995@gmail.com>
2022-11-17 13:58:11 +0200

    Add test for Rockets

---
+++ src/modules/__tests__/rockets.test.js
@@ -0,0 +1,19 @@
+import renderer from 'react-test-renderer';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Rockets from '../rockets';
+import store from '../../redux/configureStore';
+
+describe('Jest Snapshot testing suite', () => {
+  it('Matches DOM Snapshot', () => {
+    console.log('testssssssssssss');
+    const quotes = renderer.create(
+      <Provider store={store}>
+        <Router>
+          <Rockets />
+        </Router>
+      </Provider>,
+    ).toJSON();
+    expect(quotes).toMatchSnapshot();
+  });
+});
